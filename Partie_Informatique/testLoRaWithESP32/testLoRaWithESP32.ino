2cd15928437b980e87a82ec338567e82557cf26f
phelelep <133580656+phelelep@users.noreply.github.com>
2026-02-19 11:45:29 +0100

    Seance 12

--- Partie_Informatique/testLoRaWithESP32/testLoRaWithESP32.ino
+++ Partie_Informatique/testLoRaWithESP32/testLoRaWithESP32.ino
@@ -1,27 +1,33 @@
 #include <RadioLib.h>
-// NSS: 5, DIO1: 33, NRST: 27, BUSY: 35
-LLCC68 radio = new Module(5, 34, 14, 35);
+
+#define LORA_NSS   5
+#define LORA_DIO1  34
+#define LORA_NRST  27
+#define LORA_BUSY  35
+
+LLCC68 radio = new Module(LORA_NSS, LORA_DIO1, LORA_NRST, LORA_BUSY);
 
 void setup() {
   Serial.begin(115200);
-  delay(2000); // Laisse le temps au port série de s'ouvrir
+  delay(400);
 
-  Serial.println(F("\n--- Test de communication ESP32 <-> LoRa CC68 ---"));
-  Serial.print(F("[LLCC68] Initialisation... "));
+  Serial.print(F("[LLCC68] Initialising... "));
 
- int state = radio.begin(868.0, 125.0, 9, 7, 0x12, 10, 8, 1.6);
+  pinMode(LORA_BUSY, INPUT);
+  pinMode(LORA_DIO1, INPUT);
+
+  int state = radio.begin(868.0, 125.0, 9, 7, 0x12, 10, 8);  // 0.0 = XTAL
+
   if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("SUCCÈS !"));
-    Serial.println(F("Le module LoRa répond et le câblage SPI est correct."));
+    Serial.println(F("SUCCESS!"));
   } else {
-    Serial.print(F("ÉCHEC. Code erreur : "));
+    Serial.print(F("FAILED, code "));
     Serial.println(state);
-
-    while (true); // Bloque ici si ça ne marche pas
+    while (true);
   }
 }
 
 void loop() {
-  Serial.println(F("Module opérationnel. En attente de commandes..."));
-  delay(10000);
+  Serial.println(F("Ready!"));
+  delay(5000);
 }
